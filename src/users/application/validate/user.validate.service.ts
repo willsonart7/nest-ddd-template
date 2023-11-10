def7af4d01b55b40bd286c864c733177457a4ac8
Willson Arteaga <willsonart7@gmail.com>
2023-11-09 21:31:38 -0300

    refactor: validate password with aggregate root

--- src/users/application/validate/user.validate.service.ts
+++ src/users/application/validate/user.validate.service.ts
@@ -1,26 +1,21 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { User } from '../../domain/user';
+import { User, UserResponseProps } from '../../domain/user';
 import { UserRepository } from '../../domain/user.repository';
 import { Nullable } from '../../../shared/domain/Nullable';
 import { UserUsername } from '../../domain/user.username';
-import { UserPassword } from '../../domain/user.password';
-
-type ValidateReturn = {
-	id: string;
-	username: string;
-	email: string;
-};
 
 @Injectable()
 export class UserValidateService {
 	constructor(@Inject('IUserRepository') private readonly repository: UserRepository) {}
 
-	async execute(username: string, password: string): Promise<Nullable<ValidateReturn>> {
+	async execute(username: string, passwordRaw: string): Promise<Nullable<UserResponseProps>> {
 		const user: Nullable<User> = await this.repository.findByUsername(UserUsername.create(username));
 
 		if (!user) return null;
 
-		const userPassword = UserPassword.fromHashed(user.password.getValue());
-		return userPassword.compare(password) ? user.toReponse() : null;
+		const validatePassword = user.comparePassword(passwordRaw);
+		if (!validatePassword) return null;
+
+		return user.toReponse();
 	}
 }
